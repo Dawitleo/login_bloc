9da1f19dbb4b413f2a8f5722a2fd8eafd444cec9
Dawit <dawitgebeta@gmail.com>
2019-09-03 14:47:16 +0300

    Validators for fields

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -4,9 +4,11 @@ class Bloc{
   final _email = StreamController<String>();
   final _password= StreamController<String>();
 
+  //Add data to stream
   Stream<String> get email=> _email.stream;
   Stream<String> get password => _password.stream;
 
+  //Retreive data from stream
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
 
