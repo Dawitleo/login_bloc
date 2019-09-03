a8072ec752616dc3e71ac9f113a4a526b4e25217
Dawit <dawitgebeta@gmail.com>
2019-09-03 15:26:17 +0300

    Stream builder

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,8 +6,15 @@ class App extends StatelessWidget {
   build(context) {
     return MaterialApp(
       title: 'Log Me In',
-      home: Scaffold(
-        body: LoginScreen(),
+      home: Scaffold(        
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(bottom: 60.0),
+              ),
+            LoginScreen(),
+          ],
+        ),
       ),
     );
   }
--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
 
 class LoginScreen extends StatelessWidget{
 
@@ -17,22 +18,37 @@ class LoginScreen extends StatelessWidget{
   }
 
   Widget emailField(){
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        hintText: 'you@example.com',
-        labelText: 'Email Address',
-      ),
-    );
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot){
+      return TextField(
+        onChanged: bloc.changeEmail,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'you@example.com',
+          labelText: 'Email Address',
+          errorText: snapshot.error,
+          ),
+        );
+      },
+    );    
   }
 
   Widget passwordField(){
-    return TextField(
-      obscureText: false,
-      decoration: InputDecoration(
-        hintText: '********',
-        labelText: 'Password',
-      ),
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot){
+        return TextField(
+          onChanged: bloc.changePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: "****",
+            labelText: "Password",
+            errorText: snapshot.error,
+          ),
+        );
+
+      },
     );
   }
 
